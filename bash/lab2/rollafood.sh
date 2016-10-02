7be8bfa5f5dd1a08158c5d4a8e79d70a178d3d73
Dennis Simpson <dennis@zonzorp.net>
2016-10-02 19:53:51 +0000

    Improved rollafood with comments

--- bash/lab2/rollafood.sh
+++ bash/lab2/rollafood.sh
@@ -5,9 +5,16 @@
 #Add the two numbers together and use the sum as an index to 
 #display a food from your array
 
+# create array of foods
 foods=(apple banana pizza wings beer steak sandwich "pop tart" chicken ribs dirt)
-die1=$((1+ $RANDOM %6))
-die2=$((1+ $RANDOM %6))
-dicetotal=$((die1 + die2))
-foodindex=$((dicetotal - 2))
-echo "Yum, I rolled $dicetotal which gives me ${foods[$foodindex]}!"
+# roll 2 standard six-sided dice
+die1=$(($RANDOM %6))
+die2=$(($RANDOM %6))
+# the food index is 0-10
+foodindex=$((die1 + die2))
+# for dicetotal, have to add 2 because the remainders are in the range of 0-5 and dice go 1-6
+dicetotal=$((die1 + die2 + 2))
+# look up the data first to make the output command more readable
+$food=${foods[$foodindex]}
+# everything done, produce output
+echo "Yum, I rolled $dicetotal which gives me $food!"
